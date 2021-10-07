137c94609857edc31713982b961df6013cbe9944
mifidon96 <86232639+mifidon96@users.noreply.github.com>
2021-10-07 15:48:20 +0100

    Create Ex_3.py

---
+++ 1 Intro exercises (math operators)/Ex_3.py
@@ -0,0 +1,6 @@
+unit = input("Are your units feet or meters?: ")
+length = float(input("please enter the length of the room: "))
+width = float(input("please enter the width of the room: "))
+area = str(length + width)
+
+print("The area of the room is " + area + " " + unit + "^2")
