db98a549f2d97445caf2777976beeac045cf676a
mifidon96 <86232639+mifidon96@users.noreply.github.com>
2021-10-08 12:59:45 +0100

    Create Ex_9.py

---
+++ 1 Intro exercises (math operators)/Ex_9.py
@@ -0,0 +1,10 @@
+
+d = float(input("Input deposit: "))
+imulti = 1.04
+i1 = d * imulti
+i2 = i1 * imulti
+i3 = i2 * imulti
+
+print("\nBalance after 1 year: %.2f" %i1 )
+print("\Balance after 2 years: %.2f" %i2 )
+print("\Balance after 3 years: %.2f" %i3 )
