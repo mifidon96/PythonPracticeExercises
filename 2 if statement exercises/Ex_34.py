3056af21a458c9347c7df3b5f9a2feb0b3f88239
mifidon96 <86232639+mifidon96@users.noreply.github.com>
2021-10-11 09:35:49 +0100

    Create Ex_34.py

---
+++ 2 if statement exercises/Ex_34.py
@@ -0,0 +1,8 @@
+# program that determines if an integer is odd or even
+
+x = int(input("Please enter an integer: "))
+
+if x % 2 == 0:
+    print("The number is even")
+else:
+    print("The number is odd")
