f641dd9a3588d937bd0ea94b2e3ebf76322a1779
mifidon96 <86232639+mifidon96@users.noreply.github.com>
2021-10-11 09:17:22 +0100

    Create Ex_21.py

---
+++ 1 Intro exercises (math operators)/Ex_21.py
@@ -0,0 +1,8 @@
+# Program that determines the area of a triangle
+
+b = float(input("Please enter a length for the base of the triangle: "))
+h = float(input("Please enter the height of the triangle: "))
+
+a = (b*h)/2
+
+print("area of triangle: %.2f" % a)
